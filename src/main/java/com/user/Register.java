4293d803b19d8ec35537b8309c1d3ec0cbf7a976
Dinakar <103622152+DINAKAR99@users.noreply.github.com>
2025-07-07 11:22:55 +0530

    Update Register.java

--- src/main/java/com/user/Register.java
+++ src/main/java/com/user/Register.java
@@ -25,7 +25,7 @@ public class Register extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: ").append(request.getContextPath());--cloud 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
