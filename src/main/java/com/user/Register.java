c39241190088ba7c91ce69dd5b6d515c0fd9b41c
Dinakar <103622152+DINAKAR99@users.noreply.github.com>
2025-07-07 14:21:26 +0530

    Update Register.java

--- src/main/java/com/user/Register.java
+++ src/main/java/com/user/Register.java
@@ -26,7 +26,7 @@ public class Register extends HttpServlet {
 			throws ServletException, IOException {
 
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath()+"remoteee"); 
+		response.getWriter().append("Served at: ").append(request.getContextPath()+"cloud changes"); 
 	}
 
 	 
